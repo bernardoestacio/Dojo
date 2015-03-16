609cbed7d5dfba9557b12133b51cec843bd78243
bernardoestacio <bernardojses@gmail.com>
2015-03-16 01:11:58 -0300

    Terceiro Exercício

---
+++ Dojo-Exercicio3/src/TerceiroExercicio.java
@@ -0,0 +1,19 @@
+
+public class TerceiroExercicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String palavra ="pepsi";
+		int num = 5;
+		String novaPalavra;
+		
+		novaPalavra = palavra.substring(0, num); 
+		
+		for (int i = 0; i< num; i++) { 
+	    System.out.print(novaPalavra);
+		}
+		
+	}
+
+}
